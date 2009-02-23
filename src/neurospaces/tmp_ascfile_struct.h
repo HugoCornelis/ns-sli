9efba36bd1ff040d451945725cd4de4d40e408f1
Hugo Cornelis <hugo.cornelis@gmail.com>
2009-02-23 21:13:47 +0000

    1. Removed src/neurospaces/tmp_ascfile_struct.h.

--- src/neurospaces/tmp_ascfile_struct.h
+++
@@ -1,8 +0,0 @@
-struct ascfile_type {
-  char *name; int index; struct object_type *object; short flags; short nextfields; char **extfields; float x; float y; float z; unsigned int nmsgin; Msg *msgin; Msg *msgintail; unsigned int nmsgout; Msg *msgout; Msg *msgouttail; GenesisObject *compositeobject; Element *componentof; Element *parent; Element *child; Element *next;
-  char *filename;
-  int append;
-  int flush;
-  int leave_open;
-  struct OutputGenerator *pog;
-};
