bdb1e79e9878c1fb3afc08ce9bb4b9fdb3f33152
Hugo Cornelis <hugo.cornelis@gmail.com>
2013-02-04 23:13:50 +0000

    1. Reformatting and misc readability improvements.

--- src/neurospaces/nsintegrator.c
+++ src/neurospaces/nsintegrator.c
@@ -23,114 +23,102 @@
  *   Initializes the pnsintegrator.
  */
 //------------------------------------------------------------------
-int NSGenesisInitialize(struct Neurospaces *pneuro){
-   
-
-  struct neurospaces_integrator *pnsintegrator = 
-      (struct neurospaces_integrator *)calloc(1,sizeof(struct neurospaces_integrator));
+int NSGenesisInitialize(struct Neurospaces *pneuro)
+{
+    struct neurospaces_integrator *pnsintegrator
+	= (struct neurospaces_integrator *) calloc(1, sizeof(struct neurospaces_integrator));
        
-   char	*argvar[5];
-   argvar[0] = "c_do_create";
-   argvar[1] = "neurospaces";
-   argvar[2] = "/model_container";
-   do_create(3,argvar);
+    char *argvar[5];
 
-   pnsintegrator->pelNeurospaces = 
-      (struct neurospaces_type *)GetElement("/model_container");
-    
+    argvar[0] = "c_do_create";
+    argvar[1] = "neurospaces";
+    argvar[2] = "/model_container";
 
-   if(pnsintegrator->pelNeurospaces == NULL){
+    do_create(3, argvar);
 
-     printf("Could not create Model Container\n");
-     exit(1);
+    pnsintegrator->pelNeurospaces
+	= (struct neurospaces_type *) GetElement("/model_container");
 
-   }
+    if (pnsintegrator->pelNeurospaces == NULL)
+    {
+	printf("Could not create Model Container\n");
+	exit(1);
+    }
        
-   //- if no model container yet
+    //- if no model container yet
 
-   if (!pneuro)
-   {
-       //- create one with an empty model
+    if (!pneuro)
+    {
+	//- create one with an empty model
 
-       char *ppvArgs[] =
-	   {
-	       "genesis-neurospaces-bridge",
-	       "/usr/local/neurospaces/models/library/utilities/empty_model.ndf",
-	       "empty_model.ndf",
-	       NULL,
-	       NULL,
-	   };
+	char *ppvArgs[] =
+	    {
+		"genesis-neurospaces-bridge",
+		"/usr/local/neurospaces/models/library/utilities/empty_model.ndf",
+		"empty_model.ndf",
+		NULL,
+		NULL,
+	    };
 
-       pnsintegrator->pelNeurospaces->pneuro
-	   = NeurospacesNewFromCmdLine(2, &ppvArgs[0]);
+	pnsintegrator->pelNeurospaces->pneuro
+	    = NeurospacesNewFromCmdLine(2, &ppvArgs[0]);
 
-       if (!pnsintegrator->pelNeurospaces->pneuro)
-       {
-	   fprintf(stderr,
-		   "Error initializing neurospaces model container\n");
-	   return -1;
-       }
-   }
+	if (!pnsintegrator->pelNeurospaces->pneuro)
+	{
+	    fprintf(stderr, "Error initializing neurospaces model container\n");
 
-   //- else
+	    return -1;
+	}
+    }
 
-   else
-   {
-       //- link the genesis element with the pre-existing model container
+    //- else
 
-       pnsintegrator->pelNeurospaces->pneuro = pneuro;
+    else
+    {
+	//- link the genesis element with the pre-existing model container
 
-       /// \note gosh, I had to do the same hack when integrating neurospaces
-       /// \note with genesis2/hsolve.
+	pnsintegrator->pelNeurospaces->pneuro = pneuro;
 
-       struct ParserContext *pacRoot = pneuro->pacRootContext;
+	/// \note gosh, I had to do the same hack when integrating neurospaces
+	/// \note with genesis2/hsolve.
 
-       struct ImportedFile *pifRoot
-	   = ParserContextGetImportedFile(pacRoot);
+	struct ParserContext *pacRoot = pneuro->pacRootContext;
 
-       /// \note depending on how the linking is done, there can be multiple
-       /// \note instances of neurospaces around.  The following is a hack to
-       /// \note enforce the singleton (a bit)
+	struct ImportedFile *pifRoot = ParserContextGetImportedFile(pacRoot);
+
+	/// \note depending on how the linking is done, there can be multiple
+	/// \note instances of neurospaces around.  The following is a hack to
+	/// \note enforce the singleton (a bit)
 
 /*        fprintf(stdout, "HeccerConstruct(): root import is %p\n", ImportedFileGetRootImport()); */
 
-       ImportedFileSetRootImport(pifRoot);
+	ImportedFileSetRootImport(pifRoot);
 
 /*        fprintf(stdout, "HeccerConstruct(): root import is %p\n", ImportedFileGetRootImport()); */
-   }
+    }
 
+    //- Now we cache the root context for easier referencing.
 
-   //- Now we cache the root context for easier referencing.
+    pnsintegrator->ppistCachedRoot = PidinStackParse("/");
 
-   pnsintegrator->ppistCachedRoot =  PidinStackParse("/");
+    pnsintegrator->phsleCachedRoot = PidinStackLookupTopSymbol(pnsintegrator->ppistCachedRoot);
 
-   pnsintegrator->phsleCachedRoot =  
-     PidinStackLookupTopSymbol(pnsintegrator->ppistCachedRoot);
+    //- Initializes the Heccer stuff: array + index.
+    //- The heccer array is an array of pointers
+    //- to heccer objects.
 
+    pnsintegrator->iModelRegistrations = 0;
 
+    pnsintegrator->psr = (struct SolverRegistration *) calloc(MAX_HECCERS, sizeof(struct SolverRegistration));
 
+    if (!pnsintegrator->psr)
+    {
+	fprintf(stderr, "Error initializing SolverRegistration storage\n");
+	return -1;
+    }
 
-
-   //-
-   //- Initializes the Heccer stuff: array + index.
-   //- The heccer array is an array of pointers
-   //- to heccer objects.
-   //-
-   pnsintegrator->iModelRegistrations = 0;
-
-   pnsintegrator->psr
-       = (struct SolverRegistration *)calloc(MAX_HECCERS,sizeof(struct SolverRegistration));
-
-
-   if( !pnsintegrator->psr)
-   {
-       fprintf(stderr,"Error initializing SolverRegistration storage\n");
-       return -1;
-   }
-
-  
-   //- Initialize the array for storing the heccer names.
-   //- Allocate same number as the amount for heccer pointers.
+    //- Initialize the array for storing the heccer names.
+    //- Allocate same number as the amount for heccer pointers.
 
 /*    pnsintegrator->iHeccerNames = 0; */
 
@@ -148,49 +136,45 @@ int NSGenesisInitialize(struct Neurospaces *pneuro){
 /*    { */
 /*      pnsintegrator->ppcHeccerNames[pnsintegrator->iHeccerNames++] = HARDCODED_ROOT; */
 /*    } */
-    
-   
-     char	*argvar2[5];  
-     argvar2[0] = "c_do_create";  
-     argvar2[1] = "nsintegrator";  
-     argvar2[2] = "/neurospaces_integrator";  
-     do_create(3,argvar2);  
 
-     struct nsintegrator_type *pelnsintegrator =   
-        (struct nsintegrator_type*)GetElement("/neurospaces_integrator");  
+    char *argvar2[5];  
 
-     if(!pelnsintegrator){ 
-      
-       fprintf(stderr, 
- 	      "Unable to create Neurospaces Integrator Object\n"); 
-      
-       exit(1); 
-     } 
+    argvar2[0] = "c_do_create";  
+    argvar2[1] = "nsintegrator";  
+    argvar2[2] = "/neurospaces_integrator";  
 
+    do_create(3, argvar2);  
 
-     //-----------------------------------------------------------
-     //- First set the default heccer dump selection value
+    struct nsintegrator_type *pelnsintegrator
+	= (struct nsintegrator_type*) GetElement("/neurospaces_integrator");  
 
-     pelnsintegrator->heccer_dump_selection = HECCER_DUMP_ALL;
+    if (!pelnsintegrator)
+    {
+	fprintf(stderr, "Unable to create Neurospaces Integrator Object\n"); 
 
-     pelnsintegrator->heccer_reporting_granularity = 1;
+	exit(1); 
+    } 
+
+    //-----------------------------------------------------------
+    //- First set the default heccer dump selection value
+
+    pelnsintegrator->heccer_dump_selection = HECCER_DUMP_ALL;
+
+    pelnsintegrator->heccer_reporting_granularity = 1;
 
     //------------------------------------------------------------
-   //-
-   //- Create a global heccer with default options.
-   //- 
+    //-
+    //- Create a heccer for storing default heccer options.
+    //- 
 
-   struct simobj_Heccer *pheccer = HeccerNew("Options_Only",NULL,NULL,NULL);
+    struct simobj_Heccer *pheccer = HeccerNew("Options_Only", NULL, NULL, NULL);
 
-   if(!pheccer){
+    if (!pheccer)
+    {
+	fprintf(stderr, "Error allocating Heccer for Context %s\n", "Options_Only");
 
-      fprintf(stderr,
-            "Error allocating Heccer for Context %s\n",
-            "Options_Only");
-     return -1;
-
-   }
-
+	return -1;
+    }
 
 /*    pheccer->dStep = HECCER_DEFAULT_TIME_STEP;  */
 
@@ -206,36 +190,31 @@ int NSGenesisInitialize(struct Neurospaces *pneuro){
 
 /*    pheccer->ho.iSmallTableSize = HECCER_DEFAULT_INTERPOL_INTERVAL_ENTRIES; */
 
+    //! Default values are already set for the object when allocated
+    //! The defaults are defined in "heccer/heccer.h"
 
-   //! Default values are already set for the object when allocated
-   //! The defaults are defined in "heccer/heccer.h"
+    pnsintegrator->pheccerOptions = pheccer;
 
-   pnsintegrator->pheccerOptions = pheccer;
-   //---------------------------------------------------
+    //---------------------------------------------------
 
+    //-
+    //- Initialize the message array.
+    //-
 
-   //-
-   //- Initialize the message array.
-   //-
+    pnsintegrator->ppioMsg
+	= (struct ioMsg **) calloc(NUMBER_OF_MESSAGES, sizeof(struct ioMsg *));
 
-   pnsintegrator->ppioMsg = 
-     (struct ioMsg**)calloc(NUMBER_OF_MESSAGES,
-			    sizeof(struct ioMsg*));
-   pnsintegrator->iIoMsgs = 0;
+    pnsintegrator->iIoMsgs = 0;
 
-   if( !pnsintegrator->ppioMsg ){
+    if (!pnsintegrator->ppioMsg )
+    {
+	fprintf(stderr,"Error initializing IO Message storage\n");
 
-     fprintf(stderr,"Error initializing IO Message storage\n");
-     return -1;
+	return -1;
+    }
 
-   }
+    pelnsintegrator->pnsintegrator = pnsintegrator; 
 
-
-   pelnsintegrator->pnsintegrator = pnsintegrator; 
-
-   return 1;
-
-  
-
+    return 1;
 }
 
